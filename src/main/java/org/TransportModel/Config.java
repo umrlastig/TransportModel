db9af65543c1464607b17977820822048577bf98
Juste Raimbault <juste.raimbault@ign.fr>
2025-03-31 16:55:29 +0200

    add GNU-GPL license

--- src/main/java/org/TransportModel/Config.java
+++ src/main/java/org/TransportModel/Config.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2023 Erwan Hamzaoui
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.TransportModel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -97,9 +114,10 @@ import java.io.IOException;
         @JsonProperty("bdtopo") public String[] bdtopo;
         @JsonProperty("gtfs") public GTFS gtfs;
         private NetworkFiles() {}
-        ///////////////////////////////////////////////////////////////////////////////////////////////////
-        /**  Class for GTFS configuration */
-        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /**
+         *  Class for GTFS configuration
+         **/
         public static class GTFS
         {
             @JsonProperty("stops") public String stops;
@@ -110,9 +128,10 @@ import java.io.IOException;
             @JsonProperty("transfers") public String transfers;
         }
     }
-    ///////////////////////////////////////////////////////////////////////////////////////////////////
-    /** Class for Generation files configuration */
-    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Class for Generation files configuration
+     * */
     public static class GenerationFiles
     {
         @JsonProperty("zone_shapes") public String zoneShapes;
